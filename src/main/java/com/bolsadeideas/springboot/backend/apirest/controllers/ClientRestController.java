1afa427346c994cb4138744fbe7ae4709d4b0b8b
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-09-02 17:35:21 +0200

    ClientRestController - Added @CrossOrigin annotation to allow the resources sharing with API REST.

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClientRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClientRestController.java
@@ -6,12 +6,14 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClientRestController {
